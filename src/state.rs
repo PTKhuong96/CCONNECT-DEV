5495696e24e57f1b0239a3b0b2b3e100a66943c9
PTKhuong96 <phankhuong96@gmail.com>
2022-11-10 06:36:44 +0000

    init

---
+++ src/state.rs
@@ -0,0 +1,7 @@
+use cosmwasm_std::Uint128;
+use cw_storage_plus::Item;
+
+use crate::msg::ContractInfo;
+
+pub const CONTRACT_INFO: Item<ContractInfo> = Item::new("contract_info");
+pub const MINTING_FEES_INFO: Item<Option<Uint128>> = Item::new("minting_fees");
